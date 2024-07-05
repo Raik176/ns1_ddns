6506291a9ec1e20bb39b21e6e23fba69fd23ebfa
righthandman176@proton.me <raikmaus321@gmail.com>
2024-07-06 01:34:50 +0200

    Add very very simple api

--- main.go
+++ main.go
@@ -19,9 +19,22 @@ type DNSRecord struct {
 	UseClientSubnet  bool       `json:"use_client_subnet"`
 	Answers          []Answer   `json:"answers"`
 }
-
 type Answer struct {
 	Answer []string `json:"answer"`
+}
+
+type ZoneInfo struct {
+	Name             string     `json:"name"`
+	Records          []Record     `json:"records"`
+}
+type Record struct { // Don't really need any of this data
+	
+}
+
+type APIResponse struct {
+	PublicIP         string    `json:"public_ip"`
+	ZoneName         string    `json:"zone_name"`
+	RecordCount      int       `json:"record_count"`
 }
 
 const (
@@ -35,6 +48,7 @@ func main() {
 	zone := os.Getenv("NS1_ZONE")
 	key := os.Getenv("NS1_KEY")
 	interval := os.Getenv("NS1_INTERVAL")
+	disable_api := os.Getenv("NS1_API_DISABLE")
 
 	if zone == "" || key == "" {
 		fmt.Println("Missing required environment variables NS1_KEY and NS1_ZONE.")
@@ -49,8 +63,65 @@ func main() {
 		fmt.Printf("Missing or invalid NS1_INTERVAL, setting it to %d (default).\n", defaultInterval)
 		intervalMinutes = defaultInterval
 	}
-
+	
 	client := &http.Client{}
+
+	if disable_api != "true" { // TODO: add better api sometime
+		go func() {
+			http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+				if r.Method == http.MethodGet {
+					req, err := http.NewRequest("GET", baseURL + r.URL.Query().Get("zone"), nil)
+					if err != nil {
+						fmt.Printf("Error during api call : %v\n", err)
+						return
+					}
+					req.Header.Set("X-NSONE-Key", key)
+					req.Header.Set("Content-Type", "application/json")
+			
+					resp, err := client.Do(req)
+					if err != nil {
+						fmt.Printf("Error during api call : %v\n", err)
+						return
+					}
+					defer resp.Body.Close()
+
+					body, err := ioutil.ReadAll(resp.Body)
+					if err != nil {
+						fmt.Printf("API: Error reading response body: %v\n", err)
+						http.Error(w, "API: Failed to read response body", http.StatusInternalServerError)
+						return
+					}
+				
+					var zoneInfo ZoneInfo
+					err = json.Unmarshal(body, &zoneInfo)
+					if err != nil {
+						fmt.Printf("API: Error unmarshaling response body: %v\n", err)
+						http.Error(w, "API: Failed to parse JSON response", http.StatusInternalServerError)
+						return
+					}
+
+					var response APIResponse
+					response.ZoneName = zoneInfo.Name
+					response.RecordCount = len(zoneInfo.Records)
+					response.PublicIP = func() string {
+						if value := os.Getenv("NS1_PUBIP"); value != "" {
+							return value
+						}
+						return getPubIP()
+					}()
+
+					w.Header().Set("Content-Type", "application/json")
+					err = json.NewEncoder(w).Encode(response)
+					if err != nil {
+						fmt.Printf("API: Error encoding response to JSON: %v\n", err)
+						http.Error(w, "API: Failed to encode response to JSON", http.StatusInternalServerError)
+						return
+					}
+				}
+			})
+			http.ListenAndServe(":80", nil)
+		}()
+	}
 
 	UpdateDNS(domains, zone, key, client)
 	ticker := time.NewTicker(time.Duration(intervalMinutes) * time.Minute)
